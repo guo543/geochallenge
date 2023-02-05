a0474bdb36e1608058e943a9d160150cb408c829
Robert Riggs <riggsr@purdue.edu>
2023-02-05 12:54:12 -0500

    Added a navbar with placeholder links

---
+++ client/src/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+      <nav className="Navbar">
+          <a href="/">Home</a>
+          <a href="/">Game</a>
+          <a href="/">Leaderboard</a>
+          <a href="/">Upload</a>
+          <a href="/">Profile</a>
+          <a href="/">Login</a>
+          <a href="/">Logout</a>
+      </nav>
+  );
+}
+
+export default Navbar;
