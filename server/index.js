534d777b9c3b51c29571c8167ec801f91b26553e
guo543 <guo543@purdue.edu>
2023-02-05 16:06:06 -0500

    set up local mongodb connection

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.js'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,5 +18,16 @@ app.get('/hello', (req, res) => {
     res.send("hello");
 });
 
-let PORT = process.env.PORT || 8000;;
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+let PORT = process.env.PORT || 8000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+if (CONNECTION_URL === undefined) {
+    console.log("Please specify the mongodb connection string in .env");
+    process.exit(-1);
+}
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
