c3ddc89ae6faac66fd87505a924637458febd171
K Xiong <54641288+KXiong2001@users.noreply.github.com>
2023-02-15 14:12:44 -0500

    1. add routing to game page for start button. 2. Add buttons w/ place holders on main page

---
+++ client/src/titleBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./titleBar.css";
+import { useNavigate } from "react-router-dom";
+
+export default function TitleBar() {
+    const navigate = useNavigate();
+    return (
+        <section className="parent">
+            <p className="title-text" style={{ color: "white", fontSize: 24, fontWeight: 'bold'}}>
+              GeoChallenge
+            </p>
+            <div className="button-group">
+              <button className="home-button" style={{fontWeight: 'bold'}} onClick={() => navigate("/")}>
+                Home
+              </button>
+              <button className="leaderboard-button" style={{fontWeight: 'bold'}} onClick={() => navigate("/leaderboardPage")}>
+                LeaderBoard
+              </button>
+              <button className="upload-button" style={{fontWeight: 'bold'}} onClick={() => navigate("/uploadPage")}>
+                Upload
+              </button>
+              <button className="profile-button" style={{fontWeight: 'bold'}} onClick={() => navigate("/profilePage")}>
+                Profile
+              </button>
+    
+            </div>
+          </section>)
+}
