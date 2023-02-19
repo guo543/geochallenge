51cf9d6b91cded551f568d634637e61cf6481ae7
K Xiong <54641288+KXiong2001@users.noreply.github.com>
2023-02-19 16:18:15 -0500

    Fetch secret upon deployment (#13)
    * create .env for server when deploying
    
    * add manual trigger
    
    * add trigger
    
    * bug fix
    
    * take back deploy stage
    
    * remove debug

---
+++ server/scripts/create_env.sh
@@ -0,0 +1,34 @@
+
+> .env
+
+status=0
+
+write_secret_to_env() {
+    if [ "$#" -ne 2 ]; then
+        echo "Invalid numbers of parameters passed to write_secret_to_env. Required: 1"
+        status=1
+    fi
+
+    secret_name="$1"
+    eval secret_content="\$$secret_name"
+    env_name="$2"
+
+    if [[ "$secret_content" != "" && "$secret_content" != null ]]; then
+        echo "$env_name = $secret_content" >> .env
+        echo "Secret $secret_name written to .env"
+    else
+        echo "ERROR: secret $secret_name is empty or null"
+        status=1
+    fi
+}
+
+# Required
+write_secret_to_env "MONGODB_CONNECTION_URL" "CONNECTION_URL"
+
+# Not required
+if [[ $MONGODB_PORT != "" && $MONGODB_PORT != null ]]; then
+    write_secret_to_env "MONGODB_PORT" "PORT"
+fi
+
+echo "Process ended with status: $status"
+exit $status
