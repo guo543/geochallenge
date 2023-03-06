0071bc7c61d3b296de9e9d71d14696ba1d13e118
Robert Riggs <riggsr@purdue.edu>
2023-03-06 14:34:50 -0500

    Profile Page shows email

--- client/src/ProfilePage.js
+++ client/src/ProfilePage.js
@@ -1,32 +1,25 @@
 import React from "react";
-import "./MainPage.css";
+import "./ProfilePage.css";
 
 const MainPage = () => {
-  const handleStartClick = () => {};
-  return (
-    <div className="main-page-container">
-      <div className="middle-container">
-        <div className="left-side">
-          <h3 style={{ color: "#C2B04A", fontSize: 30 }}>
-            The Ultimate Purdue Geography Gaming
-          </h3>
-          <p style={{ color: "white", fontSize: 17 }}>
-            This will be the profile page.
-          </p>
-          <button className="start-button" onClick={handleStartClick}>
-            Start
-          </button>
+    return (
+        <div className="main-page-container">
+            <div className="middle-container">
+                <div className="left-side">
+                    <h3 style={{ color: "#C2B04A", fontSize: 30 }}>
+                        Email: {localStorage.getItem("userCredentials").substring(localStorage.getItem("userCredentials").indexOf("email") + 8, localStorage.getItem("userCredentials").indexOf("password") - 3)}
+                    </h3>
+                </div>
+                <div className="right-side">
+                    <img
+                        className="purdue-campus"
+                        src={require("./assets/Purdue Campus.jpg")}
+                        alt="Purdue Campus"
+                    />
+                </div>
+            </div>
         </div>
-        <div className="right-side">
-          <img
-            className="purdue-campus"
-            src={require("./assets/Purdue Campus.jpg")}
-            alt="Purdue Campus"
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MainPage;
