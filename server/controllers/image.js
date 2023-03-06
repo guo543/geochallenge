03343855bbcca688fdd60e9abe3e683d14b24c25
guo543 <guo543@purdue.edu>
2023-03-05 19:07:53 -0500

    fix typo

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,7 +1,7 @@
 import Image from "../models/image.js";
 
 export const uploadImage = async (req, res) => {
-    const result = await Image.create({ name: "test", numReports: 5, localtion: "test location" });
+    const result = await Image.create({ name: "test", numReports: 0, location: "test location" });
 
     res.status(200).json({ result: result });
 }
