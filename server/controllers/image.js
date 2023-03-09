96a6d1f1a28c0f7f78c596ac7a95aa9857270e49
guo543 <guo543@purdue.edu>
2023-03-08 22:16:54 -0500

    add API for reporting images

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,4 +1,5 @@
 import Image from "../models/image.js";
+import mongoose from "mongoose";
 
 export const uploadImage = async (req, res) => {
     const result = await Image.create({
@@ -8,4 +9,19 @@ export const uploadImage = async (req, res) => {
     });
 
     res.status(200).json({ result: result });
+}
+
+export const reportImage = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No image with that id');
+
+    const image = await Image.findById(id);
+
+    console.log(image);
+    
+    image.numReports = image.numReports + 1;
+    const updatedImage = await Image.findByIdAndUpdate(id, image, { new: true });
+
+    res.send(updatedImage);
 }
