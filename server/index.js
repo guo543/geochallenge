4a12a02fc497e97549f7989da5f21966b3983010
K Xiong <54641288+KXiong2001@users.noreply.github.com>
2023-03-20 15:16:04 -0400

    Revert "Attempt to fix cors (#40)"
    This reverts commit 188b4538b31dee08dfd78168b653c98a6bac6889.

--- server/index.js
+++ server/index.js
@@ -41,14 +41,6 @@ if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 }
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next();
-  });
-
 export default app;
 
 
