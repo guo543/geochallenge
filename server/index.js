bf59b3399a7bdfdffb2e4bafae6fc6badbb89b10
K Xiong <54641288+KXiong2001@users.noreply.github.com>
2023-03-21 20:27:46 -0400

    bring use cors to front. revert endpoint change
    Revert fix

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ import auth from './middleware/auth.js';
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
 app.use('/user', userRoutes);
 app.use('/image', imageRoutes);
